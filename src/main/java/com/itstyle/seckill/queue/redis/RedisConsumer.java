8835e9042aed6cfa4554a39bf3716ebfe5d8e3a6
小柒2012 <345849402@qq.com>
2020-12-09 21:39:48 +0800

    :bug:消息推送BUG修复

--- src/main/java/com/itstyle/seckill/queue/redis/RedisConsumer.java
+++ src/main/java/com/itstyle/seckill/queue/redis/RedisConsumer.java
@@ -28,13 +28,13 @@ public class RedisConsumer {
     	if(redisUtil.getValue(array[0])==null){//control层已经判断了，其实这里不需要再判断了
     		Result result = seckillService.startSeckilDBPCC_TWO(Long.parseLong(array[0]), Long.parseLong(array[1]));
     		if(result.equals(Result.ok(SeckillStatEnum.SUCCESS))){
-    			WebSocketServer.sendInfo(array[0], "秒杀成功");//推送给前台
+    			WebSocketServer.sendInfo("秒杀成功",array[0]);//推送给前台
     		}else{
-    			WebSocketServer.sendInfo(array[0], "秒杀失败");//推送给前台
+    			WebSocketServer.sendInfo("秒杀失败",array[0]);//推送给前台
     			redisUtil.cacheValue(array[0], "ok");//秒杀结束
     		}
     	}else{
-    		WebSocketServer.sendInfo(array[0], "秒杀失败");//推送给前台
+    		WebSocketServer.sendInfo("秒杀失败",array[0]);//推送给前台
     	}
     }
 }
